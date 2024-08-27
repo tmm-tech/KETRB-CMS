0b303deda0b223e1afb800ec72f2cefc61a66a02
tmm-tech <tonymugi074@gmail.com>
2024-08-27 23:40:22 +0300

    KETRB

--- ketrb_cms/src/index.jsx
+++ ketrb_cms/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 import './index.css';
-import './Page/i18n'; 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
