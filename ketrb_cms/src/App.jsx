07da74170abfc0089cb17e97d3f665a152329a61
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-08-29 21:24:03 +0000

    KETRB CMS

--- ketrb_cms/src/App.jsx
+++ ketrb_cms/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import React from "react";
 import Dashboard from "./Page/Dashboard";
+import NewsPage from "./Page/NewsPage";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/news" element={<NewsPage />} />
       </Routes>
     </div>
   );
