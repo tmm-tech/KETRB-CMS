a899ffca5e46862d74200f0f5e3bde3f2ff0cc7d
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-08-30 08:04:32 +0300

    KETRB

--- ketrb_cms/src/Component/SideNav.jsx
+++ ketrb_cms/src/Component/SideNav.jsx
@@ -14,7 +14,7 @@ const SideNav = () => {
   const { pathname } = useLocation();
 
   return (
-    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-white sm:flex">
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
         <TooltipProvider>
           <Link
