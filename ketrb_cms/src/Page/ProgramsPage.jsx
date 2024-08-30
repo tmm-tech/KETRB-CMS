85d36e6d2c860fe478d1a130294df633199b42f2
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-08-30 10:14:55 +0300

    KETRB

--- ketrb_cms/src/Page/ProgramsPage.jsx
+++ ketrb_cms/src/Page/ProgramsPage.jsx
@@ -22,7 +22,7 @@ const ProgramsPage = () => {
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                         <Card>
                             <CardHeader>
-                                <CardTitle>News</CardTitle>
+                                <CardTitle>Programs</CardTitle>
                                 <CardDescription>Manage your news articles</CardDescription>
                             </CardHeader>
                             <CardContent>
