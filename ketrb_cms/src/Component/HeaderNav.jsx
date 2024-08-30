7d8b3d78ede866d717eb4b5d30cbf848039977a1
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-08-30 10:11:11 +0300

    KETRB

--- ketrb_cms/src/Component/HeaderNav.jsx
+++ ketrb_cms/src/Component/HeaderNav.jsx
@@ -10,7 +10,7 @@ import logo from "../Asset/Logo/ketrb.ico";
 
 const HeaderNav = () => {
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-white px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b px-4 sm:static sm:h-auto sm:border-0 sm:bg-white sm:px-6">
       <Sheet>
         <SheetTrigger asChild>
           <Button size="icon" variant="outline" className="sm:hidden">
