3665554deb067750d92d6df884c64b965e615d60
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-08-31 06:28:25 +0000

    KETRB CMS

---
+++ ketrb_cms/src/Component/label.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+interface LabelProps extends React.HTMLAttributes<HTMLLabelElement>, VariantProps<typeof labelVariants> {}
+
+const labelVariants = cva("your-base-styles", {
+  variants: {
+    // Define your variants here
+  },
+});
+
+const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
+  ({ className, ...props }, ref) => (
+    <label ref={ref} className={labelVariants({ className })} {...props} />
+  )
+);
+
+Label.displayName = "Label";
+
+export default Label;
