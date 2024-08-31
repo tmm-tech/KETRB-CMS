4b93eb1f7e3e3097c09c64b22b1a5f8b21649a88
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-08-31 07:47:25 +0000

    KETRB CMS

--- ketrb_cms/src/Page/NewsAdd.jsx
+++ ketrb_cms/src/Page/NewsAdd.jsx
@@ -17,7 +17,7 @@ const NewsAdd = () => {
         <HeaderNav />
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 w-full">
           <div
-            className="mx-auto grid max-w-[100vw] grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            className="w-full grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             <Card>
               <CardHeader>
                 <CardTitle>News</CardTitle>
