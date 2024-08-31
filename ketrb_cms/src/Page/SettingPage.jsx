98e6a71d619bd7540a06201d9d57cb1308eacdf4
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-08-31 20:07:41 +0000

    KETRB CMS

--- ketrb_cms/src/Page/SettingPage.jsx
+++ ketrb_cms/src/Page/SettingPage.jsx
@@ -12,7 +12,7 @@ const SettingPage = () => {
         <div className="flex min-h-screen w-full flex-col bg-muted/40">
             <SideNav />
             <div
-                className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14"
+                className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 mt-20"
                 style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
             >
                 <HeaderNav />
