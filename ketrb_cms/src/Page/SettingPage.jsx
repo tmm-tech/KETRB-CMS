e7d7ad460217915ffda9e75001a096e4c386b1f1
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-08-31 19:53:28 +0000

    KETRB CMS

--- ketrb_cms/src/Page/SettingPage.jsx
+++ ketrb_cms/src/Page/SettingPage.jsx
@@ -16,10 +16,10 @@ const SettingPage = () => {
                 style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
             >
                 <HeaderNav />
-                <div className="flex min-h-screen bg-muted">
+                <div className="flex items-center justify-center min-h-screen bg-muted">
                     <Tabs className="flex-1 grid md:grid-cols-[240px_1fr] gap-8 p-4 md:p-8" defaultValue="general">
                         {/* Vertical Tab List */}
-                        <TabsList asChild className="mt-30">
+                        <TabsList asChild>
                             <nav className="bg-background rounded-md shadow-sm">
                                 <div className="space-y-1 p-4">
                                     <TabsTrigger value="general" asChild>
