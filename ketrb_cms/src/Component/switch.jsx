df2971d8b8762655d8d57e59014a8ab164e26e4a
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-08-31 21:51:30 +0000

    KETRB CMS

--- ketrb_cms/src/Component/switch.jsx
+++ ketrb_cms/src/Component/switch.jsx
@@ -8,7 +8,7 @@ const cn = (...classes) => classes.filter(Boolean).join(' ');
 const Switch = React.forwardRef(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black-500 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-black-600 data-[state=unchecked]:bg-gray-300",
+      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-blue-600 data-[state=unchecked]:bg-gray-300",
       className
     )}
     {...props}
