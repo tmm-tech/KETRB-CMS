e5a8caf6aa33bc0ac9d58fab2a2607115bd69e21
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-05 18:05:45 +0000

    KETRB CMS

--- ketrb_cms/src/Component/alert-dialog.jsx
+++ ketrb_cms/src/Component/alert-dialog.jsx
@@ -1,10 +1,7 @@
-"use client"
-
 import * as React from "react"
-import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
-
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
+import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
+import { cn } from "../lib/utils";
+import { buttonVariants } from "../Component/button";
 
 const AlertDialog = AlertDialogPrimitive.Root
 
