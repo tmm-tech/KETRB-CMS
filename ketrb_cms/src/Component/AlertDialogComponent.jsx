fbb2e2e8f528a26cb52ac9cde6e0c3d5ed7ae086
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-06 11:11:47 +0300

    KETRB CMS

--- ketrb_cms/src/Component/AlertDialogComponent.jsx
+++ ketrb_cms/src/Component/AlertDialogComponent.jsx
@@ -1,32 +1,33 @@
 import React from "react";
-import {
-    AlertDialog,
-    AlertDialogContent,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogCancel,
-    AlertDialogAction
-} from "../Component/alert-dialog";
-import { Link } from "react-router-dom";
 
 const AlertDialogComponent = ({ isOpen, onClose, message }) => {
+    if (!isOpen) return null;
+
     return (
-        <AlertDialog open={isOpen} onOpenChange={onClose}>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>{message}</AlertDialogTitle>
-                    <AlertDialogDescription>
-                        {/* Description can be customized based on the message */}
-                    </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogCancel onClick={() => onClose(false)}>Close</AlertDialogCancel>
-                    <Link to="/users"><AlertDialogAction>Continue</AlertDialogAction></Link>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
+        <div
+            className="fixed inset-0 bg-black/50 flex items-center justify-center"
+            onClick={() => onClose(false)}
+        >
+            <div
+                className="bg-white rounded-lg shadow-lg w-full max-w-lg p-6 relative"
+                onClick={(e) => e.stopPropagation()}
+            >
+                <div className="border-b border-gray-200 pb-4">
+                    <h3 className="text-xl font-semibold">Notification</h3>
+                </div>
+                <div className="py-4">
+                    <p>{message}</p>
+                </div>
+                <div className="flex justify-end pt-4 border-t border-gray-200">
+                    <button
+                        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
+                        onClick={() => onClose(false)}
+                    >
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
     );
 };
 
