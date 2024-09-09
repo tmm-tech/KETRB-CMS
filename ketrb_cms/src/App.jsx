8f011c6b9ac87469b8aba988f2136ead1bbead73
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-09 20:09:09 +0000

    KETRB CMS

--- ketrb_cms/src/App.jsx
+++ ketrb_cms/src/App.jsx
@@ -1,35 +1,38 @@
-import { Route, Routes } from "react-router-dom";
-import React from "react";
-import Dashboard from "./Page/Dashboard";
-import NewsPage from "./Page/NewsPage";
-import ImagePage from "./Page/ImagePage";
-import ProgramsPage from "./Page/ProgramsPage";
-import UserPage from "./Page/UserPage";
-import LoginPage from "./Page/LoginPage";
-import NewsAdd from "./Page/NewsAdd";
-import ProgramsAdd from "./Page/ProgramsAdd";
-import UserAdd from "./Page/UsersAdd";
-import ProfilePage from "./Page/ProfilePage";
+import { Route, Routes, Navigate } from 'react-router-dom';
+import React from 'react';
+import Dashboard from './Page/Dashboard';
+import NewsPage from './Page/NewsPage';
+import ImagePage from './Page/ImagePage';
+import ProgramsPage from './Page/ProgramsPage';
+import UserPage from './Page/UserPage';
+import LoginPage from './Page/LoginPage';
+import NewsAdd from './Page/NewsAdd';
+import ProgramsAdd from './Page/ProgramsAdd';
+import UserAdd from './Page/UsersAdd';
+import ProfilePage from './Page/ProfilePage';
 
-const isAuthenticated = () => {
-  const token = localStorage.getItem('token');
-  return !!token;
-};
+const isAuthenticated = () => !!localStorage.getItem('token');
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/news/add news" element={<NewsAdd />} />
-        <Route path="/images" element={<ImagePage />} />
-        <Route path="/programs" element={<ProgramsPage />} />
-        <Route path="/programs/add program" element={<ProgramsAdd />} />
-        <Route path="/users" element={<UserPage />} />
-        <Route path="/users/add users" element={<UserAdd />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        {isAuthenticated() ? (
+          <>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/news" element={<NewsPage />} />
+            <Route path="/news/add-news" element={<NewsAdd />} />
+            <Route path="/images" element={<ImagePage />} />
+            <Route path="/programs" element={<ProgramsPage />} />
+            <Route path="/programs/add-program" element={<ProgramsAdd />} />
+            <Route path="/users" element={<UserPage />} />
+            <Route path="/users/add-users" element={<UserAdd />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </>
+        ) : (
+          <Route path="*" element={<Navigate to="/login" />} />
+        )}
       </Routes>
     </div>
   );
