55722acb475135ff9659675fa1b0c73600034e75
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-09 20:03:46 +0000

    KETRB CMS

--- ketrb_cms/src/App.jsx
+++ ketrb_cms/src/App.jsx
@@ -1,41 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Dashboard from './Page/Dashboard';
-import NewsPage from './Page/NewsPage';
-import ImagePage from './Page/ImagePage';
-import ProgramsPage from './Page/ProgramsPage';
-import UserPage from './Page/UserPage';
-import LoginPage from './Page/LoginPage';
-import NewsAdd from './Page/NewsAdd';
-import ProgramsAdd from './Page/ProgramsAdd';
-import UserAdd from './Page/UsersAdd';
-import ProfilePage from './Page/ProfilePage';
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+import Dashboard from "./Page/Dashboard";
+import NewsPage from "./Page/NewsPage";
+import ImagePage from "./Page/ImagePage";
+import ProgramsPage from "./Page/ProgramsPage";
+import UserPage from "./Page/UserPage";
+import LoginPage from "./Page/LoginPage";
+import NewsAdd from "./Page/NewsAdd";
+import ProgramsAdd from "./Page/ProgramsAdd";
+import UserAdd from "./Page/UsersAdd";
+import ProfilePage from "./Page/ProfilePage";
 
-// Function to check if the user is authenticated
-const isAuthenticated = () => !!localStorage.getItem('token');
-
-// Helper function to protect routes
-const ProtectedRoute = ({ element }) => (
-    isAuthenticated() ? element : <Navigate to="/login" />
-);
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  return !!token;
+};
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/" element={<ProtectedRoute element={<Dashboard />} />} />
-                <Route path="/news" element={<ProtectedRoute element={<NewsPage />} />} />
-                <Route path="/news/add-news" element={<ProtectedRoute element={<NewsAdd />} />} />
-                <Route path="/images" element={<ProtectedRoute element={<ImagePage />} />} />
-                <Route path="/programs" element={<ProtectedRoute element={<ProgramsPage />} />} />
-                <Route path="/programs/add-program" element={<ProtectedRoute element={<ProgramsAdd />} />} />
-                <Route path="/users" element={<ProtectedRoute element={<UserPage />} />} />
-                <Route path="/users/add-users" element={<ProtectedRoute element={<UserAdd />} />} />
-                <Route path="/profile" element={<ProtectedRoute element={<ProfilePage />} />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/news/add news" element={<NewsAdd />} />
+        <Route path="/images" element={<ImagePage />} />
+        <Route path="/programs" element={<ProgramsPage />} />
+        <Route path="/programs/add program" element={<ProgramsAdd />} />
+        <Route path="/users" element={<UserPage />} />
+        <Route path="/users/add users" element={<UserAdd />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
