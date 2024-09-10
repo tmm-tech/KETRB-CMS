13cba882691e2670c9cc0df73a24707a1e174791
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-10 13:50:28 +0300

    KETRB CMS

--- ketrb_cms/src/Page/LoginPage.jsx
+++ ketrb_cms/src/Page/LoginPage.jsx
@@ -12,7 +12,10 @@ const LoginPage = () => {
     e.preventDefault();
     setLoading(true);
     setError(null);
-
+const data = {
+            email,
+            password
+        };
 const response = await fetch('https://ketrb-backend.onrender.com/users/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
