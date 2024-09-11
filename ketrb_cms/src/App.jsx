0636ddd993e1f57065d2db113762ab2841ee4fc0
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-11 13:48:00 +0300

    KETRB CMS

--- ketrb_cms/src/App.jsx
+++ ketrb_cms/src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Dashboard from './Page/Dashboard';
 import NewsPage from './Page/NewsPage';
 import ImagePage from './Page/ImagePage';
@@ -11,14 +11,43 @@ import ProgramsAdd from './Page/ProgramsAdd';
 import UserAdd from './Page/UsersAdd';
 import ProfilePage from './Page/ProfilePage';
 
-const isAuthenticated = () => !!localStorage.getItem('token');
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-function App() {
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await fetch('https://ketrb-backend.onrender.com/users/check-auth', {
+          method: 'GET',
+          credentials: 'include' // Include cookies with the request
+        });
+
+        if (response.ok) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        console.error('Error checking authentication:', error);
+        setIsAuthenticated(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; // Or some loading spinner
+  }
+
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        {isAuthenticated() ? (
+        {isAuthenticated ? (
           <>
             <Route path="/" element={<Dashboard />} />
             <Route path="/news" element={<NewsPage />} />
@@ -36,6 +65,6 @@ function App() {
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
