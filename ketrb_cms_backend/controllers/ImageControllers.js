16fd9a91716a1b165743e520ebde6e4c40323274
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-14 18:15:00 +0000

    KETRB CMS

--- ketrb_cms_backend/controllers/ImageControllers.js
+++ ketrb_cms_backend/controllers/ImageControllers.js
@@ -17,7 +17,7 @@ module.exports = {
             // Decode base64 image
             const base64Data = image.replace(/^data:image\/png;base64,/, "");
             const fileName = `${Date.now()}.png`;
-            const filePath = path.join(__dirname, 'uploads', fileName);
+            const filePath = path.join(__dirname, '../uploads', fileName);
     
             // Save the image to the server
             await fs.promises.writeFile(filePath, base64Data, 'base64');
