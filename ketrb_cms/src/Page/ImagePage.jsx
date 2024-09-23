eb77454222ffe4c076a5bf2be57354b3ec1b246b
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-23 23:21:45 +0300

    Update ImagePage.jsx

--- ketrb_cms/src/Page/ImagePage.jsx
+++ ketrb_cms/src/Page/ImagePage.jsx
@@ -47,7 +47,9 @@ const ImagePage = () => {
 
     fetchImages();
   }, []);
-
+ if (imageloading) {
+    return <LoadingPage />;
+  }
   const handleCancel = () => {
     setImageFile(null);
   };
@@ -260,9 +262,7 @@ const handleDelete = async (id) => {
               </div>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-  {loading ? (
-    <LoadingPage />
-  ) : images.length === 0 ? (
+  {images.length === 0 ? (
     <div className="col-span-full flex items-center justify-center">
       <p className="text-center text-gray-500">No images available.</p>
     </div>
