620760bf2caa0db3b7bf169d48942bd979e0b1f3
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-23 23:31:35 +0300

    KETRB CMS

--- ketrb_cms_backend/routes/ImageRoutes.js
+++ ketrb_cms_backend/routes/ImageRoutes.js
@@ -19,37 +19,14 @@ if (!fs.existsSync(uploadFolderPath)) {
   fs.mkdirSync(uploadFolderPath, { recursive: true });
 }
 
-// Path to the counter file
-const counterFilePath = path.join(rootDirectory, 'counter.json');
-
-// Read and update the counter value
-const readCounter = async () => {
-  if (!fs.existsSync(counterFilePath)) {
-    // Initialize counter file if it doesn't exist
-    await promisify(fs.writeFile)(counterFilePath, JSON.stringify({ count: 0 }), 'utf8');
-  }
-  const data = await promisify(fs.readFile)(counterFilePath, 'utf8');
-  return JSON.parse(data).count;
-};
-
-const updateCounter = async (count) => {
-  await promisify(fs.writeFile)(counterFilePath, JSON.stringify({ count }), 'utf8');
-};
-
-// Multer configuration for image uploads
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadFolderPath);  // Upload to 'public/gallery'
+  destination: (req, file, cb) => {
+    cb(null, uploadFolderPath); // Use the dynamically created path
   },
-  filename: async function (req, file, cb) {
-    const ext = path.extname(file.originalname);  // Get the original file extension
-    const count = await readCounter();
-    const fileName = `kertb_img${String(count + 1).padStart(3, '0')}${ext}`;  // Incremental name
-    await updateCounter(count + 1);
-    cb(null, fileName);
-  }
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname); // Unique filename
+  },
 });
-
 // Multer middleware to handle single file upload
 const upload = multer({ storage: storage });
 
