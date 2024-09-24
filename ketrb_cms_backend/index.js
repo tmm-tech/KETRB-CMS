1bfc4a14f510c9259c3355370b7f3454b79d49de
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-24 22:40:57 +0300

    KETRB CMS

--- ketrb_cms_backend/index.js
+++ ketrb_cms_backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const path = require('path');
+const fs = require('fs');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const UserRoutes = require('./routes/UserRoutes');
