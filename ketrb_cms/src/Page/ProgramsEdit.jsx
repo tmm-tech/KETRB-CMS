4db5fad13e288de52a7a99ead4ed4ce3869aedb4
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-25 22:14:32 +0300

    Update ProgramsEdit.jsx

--- ketrb_cms/src/Page/ProgramsEdit.jsx
+++ ketrb_cms/src/Page/ProgramsEdit.jsx
@@ -157,7 +157,7 @@ const ProgramsEdit = () => {
 
                         {/* Edit icon */}
                                         <button
-                                            className="absolute top-2 right-1 text-gray-500 hover:text-gray-700"
+                                            className="top-2 right-1 text-gray-500 hover:text-gray-700"
                                             onClick={() => setEditMode(true)}
                                         >
                                            <FilePenIcon className="h-4 w-4" />
