3475b0ece15762a2ab0052bb4f9ce8ce177129dd
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-30 23:10:22 +0300

    Update pagination.jsx

--- ketrb_cms/src/Component/pagination.jsx
+++ ketrb_cms/src/Component/pagination.jsx
@@ -1,7 +1,6 @@
-import * as React from "react"
-import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
-
-import { cn } from "../lib/utils"
+import * as React from "react";
+import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react";
+import { cn } from "../lib/utils";
 import { buttonVariants } from "../Component/button";
 
 const Pagination = ({
