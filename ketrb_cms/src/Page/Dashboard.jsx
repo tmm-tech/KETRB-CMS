521365c4618cd70ac00ff31852e56108b71575e4
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-10-01 14:16:11 +0300

    Update Dashboard.jsx

--- ketrb_cms/src/Page/Dashboard.jsx
+++ ketrb_cms/src/Page/Dashboard.jsx
@@ -125,8 +125,8 @@ const Dashboard = () => {
                   </DropdownMenuContent>
                 </DropdownMenu>
                 <Button size="sm" className="h-8 gap-1 bg-black text-white">
-                  <PlusIcon className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Add</span>
+                  <ListOrderedIcon className="h-3.5 w-3.5" />
+                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Sort by</span>
                 </Button>
 
               </div>
@@ -555,6 +555,29 @@ function DownloadIcon(props) {
       <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
       <polyline points="7 10 12 15 17 10" />
       <line x1="12" x2="12" y1="15" y2="3" />
+    </svg>
+  )
+}
+function ListOrderedIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="10" x2="21" y1="6" y2="6" />
+      <line x1="10" x2="21" y1="12" y2="12" />
+      <line x1="10" x2="21" y1="18" y2="18" />
+      <path d="M4 6h1v4" />
+      <path d="M4 10h2" />
+      <path d="M6 18H4c0-1 2-2 2-3s-1-1.5-2-1" />
     </svg>
   )
 }
