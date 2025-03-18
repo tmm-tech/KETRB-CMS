388ff42c28f40ab8a75e5494b7b1011ac57bf7cb
tmm-tech <tonymugi074@gmail.com>
2025-03-18 21:08:10 +0300

    side nav

--- ketrb_cms/src/Component/SideNav.jsx
+++ ketrb_cms/src/Component/SideNav.jsx
@@ -355,10 +355,11 @@ function EmployeeIcon(props) {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-      <circle cx="9" cy="7" r="4" />
-      <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+      <rect x="2" y="7" width="20" height="14" rx="2" ry="2" />
       <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+      <path d="M8 3.13a4 4 0 0 0 0 7.75" />
+      <path d="M12 7m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+      <path d="M8 14h8v4h-8z" />
     </svg>
   )
 }
