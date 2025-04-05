a25567f728b58fa8256850bbf11cc495895f053e
tmmtech <tonymugi074@gmail.com>
2025-04-05 16:41:04 +0300

    Career

--- ketrb_cms_backend/controllers/CareerControllers.js
+++ ketrb_cms_backend/controllers/CareerControllers.js
@@ -41,8 +41,8 @@ module.exports = {
       }
       res.status(200).json(result.rows[0]);
     } catch (error) {
-      console.error('Error fetching job posting:', error);
-      res.status(500).json({ message: 'Error fetching job posting.' });
+      console.error('Error fetching a job posting:', error);
+      res.status(500).json({ message: 'Error fetching a job posting.' });
     }
   },
 
