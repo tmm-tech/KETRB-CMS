d7cecd75fde2bebf59da49a0f9d49f211da7a6cd
tmmtech <tonymugi074@gmail.com>
2025-04-05 21:24:10 +0300

    Backend

--- ketrb_cms_backend/index.js
+++ ketrb_cms_backend/index.js
@@ -29,17 +29,12 @@ const allowedOrigins = [
   ];
   
   const corsOptions = {
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization']
+    origin: true,  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
   };
+  
+  app.use(cors(corsOptions));
   
 app.use(cors(corsOptions));
 
