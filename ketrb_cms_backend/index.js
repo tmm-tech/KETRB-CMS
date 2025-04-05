4354d8cca9a72feddd0bb723d24d5794e285bce2
tmmtech <tonymugi074@gmail.com>
2025-04-05 15:08:02 +0300

    Links

--- ketrb_cms_backend/index.js
+++ ketrb_cms_backend/index.js
@@ -24,11 +24,18 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 const corsOptions = {
-  origin: 'https://ketrb-cms-one.vercel.app', // Your frontend URL
-  credentials: true, // Allow cookies to be sent
-  methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
-  allowedHeaders: 'Content-Type,Authorization' // Allowed headers
-};
+    origin: (origin, callback) => {
+      const allowedOrigins = ['https://ketrb-cms-one.vercel.app', 'https://ketrb.netlify.app'];
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, // Allow cookies to be sent
+    methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type,Authorization' // Allowed headers
+  };
 app.use(cors(corsOptions));
 
 // Body parsing
