4ac1d1479308f0a6652b3848843a418610416dc1
tmmtech <tonymugi074@gmail.com>
2025-04-06 19:42:04 +0300

    Employee

--- ketrb_cms/src/Page/EmployeesPageAdd.jsx
+++ ketrb_cms/src/Page/EmployeesPageAdd.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import SideNav from "../Component/SideNav";
 import HeaderNav from "../Component/HeaderNav";
