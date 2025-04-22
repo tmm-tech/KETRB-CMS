b13b901cea4791293af17deff5aa6c233d8d2769
tmmtech <tonymugi074@gmail.com>
2025-04-22 22:34:30 +0300

    Career

--- ketrb_cms_backend/routes/CareerRoutes.js
+++ ketrb_cms_backend/routes/CareerRoutes.js
@@ -16,29 +16,40 @@ const {
 
 const CareerRoutes = express.Router();
 
+
+// Define the root directory and the upload folder path
+const rootDirectory = path.resolve(__dirname, '../public');
+const uploadFolderPath = path.join(rootDirectory, 'resumes');
+
+// Ensure the upload folder exists
+const fs = require('fs');
+if (!fs.existsSync(uploadFolderPath)) {
+  fs.mkdirSync(uploadFolderPath, { recursive: true });
+}
+
 // Configure Multer for resume uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../public/resumes')); // Save resumes to /public/resumes
+    cb(null, uploadFolderPath);
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname); // Unique filename
-  }
+    cb(null, Date.now() + '-' + file.originalname);
+  },
 });
 const upload = multer({ storage: storage });
 
 
 // Get all careers
-CareerRoutes.get('/', GetAllCareers); 
+CareerRoutes.get('/', GetAllCareers);
 
 // Get all published career posts
-CareerRoutes.get('/careers', GetPublishedCareers); 
+CareerRoutes.get('/careers', GetPublishedCareers);
 
 // Get all applications
 CareerRoutes.get('/applications', GetAllApplications);
 
 // Apply for  a job
-CareerRoutes.post('/apply', upload.single('resume'), ApplyForJob); 
+CareerRoutes.post('/apply', upload.single('resume'), ApplyForJob);
 
 // Approve a career post
 CareerRoutes.put('/approve/:id', ApproveCareer);
